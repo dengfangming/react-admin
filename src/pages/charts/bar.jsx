18f0a5738bcd940860643379c4ad7a9fb195b277
qaq Doe <johndoe@example.com>
2019-04-11 17:29:35 +0800

    完成图形图表

--- src/pages/charts/bar.jsx
+++ src/pages/charts/bar.jsx
@@ -1,10 +1,24 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
 
+export default class Bar extends Component {
 
-export default class Bar extends Component{
-    render(){
-        return <div>
-            Bar
-        </div>
+    getOption = () => {
+        return {
+            xAxis: {
+                type: 'category',
+                data: ['衬衫1', '羊毛衫1', '雪纺衫1', '裤子1', '高跟鞋1','袜子1']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [5, 20, 35, 10, 10, 20],
+                type: 'bar'
+            }]
+        };
+    };
+    render() {
+        return <ReactEcharts option={this.getOption()}/>;
     }
 }
--- src/pages/charts/line.jsx
+++ src/pages/charts/line.jsx
@@ -1,10 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
+import ReactEcharts from 'echarts-for-react';
 
+export default class Line extends Component {
 
-export default class Line extends Component{
-    render(){
-        return <div>
-            Line
-        </div>
+    getOption = () => {
+        return {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line'
+            }]
+        };
+    };
+
+    render() {
+        return <ReactEcharts option={this.getOption()}/>;
     }
 }
